36523559d5447327615a57603670d3cfbbbe1cac
Brian Weaver <bdrums@gmail.com>
2017-05-21 08:24:42 -0400

    Updates to use MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,11 @@
 
 ActiveRecord::Schema.define(version: 20170312142534) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
