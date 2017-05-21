d67c94d8642bb57248ca1faa14af2186d6fcf3b5
Brian Weaver <bdrums@gmail.com>
2017-05-21 08:26:32 -0400

    Adds html view of query results

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,7 +2,9 @@ class GraphqlController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
   def query
-  	res = Schema.execute(params[:query], variables: params[:variables])
-		render json: res
+  	@res = Schema.execute(params[:query], variables: params[:variables])
+    @data = @res['data']
+		
+    render json: @res if request.post?
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'graphql', to: 'graphql#query', as: 'graphql_query'
+  match 'graphql', to: 'graphql#query', as: 'graphql_query', via: [:get, :post]
 	mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
